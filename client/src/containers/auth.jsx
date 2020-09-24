7e18a8aff36dc0c79f750ef38ebb9a6ca795af08
Ankur Himanshu <ankurhimanshu1408@gmail.com>
2020-09-24 12:07:12 +0530

    rework on react

--- client/src/containers/auth.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-const AuthContext = createContext();
-
-export function useAuth() {
-  return useContext(AuthContext);
-}
-
-export default AuthContext;
--- client/src/containers/rmStore/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function RMStore(props) {
-    return <div>Raw Material Store</div>;
-}
